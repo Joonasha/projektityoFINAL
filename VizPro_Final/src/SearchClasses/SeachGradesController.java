696c365f2c8307f90fb7737fb009b68a3bf056a8
Joonasha <47676026+Joonasha@users.noreply.github.com>
2019-04-24 21:16:00 +0300

    hakusivun eka osio

--- VizPro_Final/src/SearchClasses/SeachGradesController.java
+++ VizPro_Final/src/SearchClasses/SeachGradesController.java
@@ -5,9 +5,16 @@
  */
 package SearchClasses;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 
 /**
  * FXML Controller class
@@ -16,12 +23,54 @@ import javafx.fxml.Initializable;
  */
 public class SeachGradesController implements Initializable {
 
+    @FXML
+    private JFXTextField nameTF;
+    @FXML
+    private JFXTextField authorsTF;
+    @FXML
+    private JFXComboBox<?> yearCB;
+    @FXML
+    private JFXComboBox<?> typeCB;
+    @FXML
+    private JFXComboBox<?> levelCB;
+    @FXML
+    private JFXComboBox<?> languageCB;
+    @FXML
+    private JFXComboBox<?> authorsCB;
+    @FXML
+    private VBox vbox1;
+    @FXML
+    private VBox vbox2;
+    @FXML
+    private VBox vboxMain;
+    @FXML
+    private AnchorPane anchorpane;
+    @FXML
+    private JFXButton ParadigmaButton;
+    @FXML
+    private VBox content1;
+    @FXML
+    private VBox content2;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        vbox1.setStyle("-fx-background-color:black");
+        vbox2.setStyle("-fx-background-color:black");
+        anchorpane.setStyle("-fx-background-color:black");
+        vboxMain.setStyle("-fx-background-color:white");
+        
+        
+        
+        
+        
     }    
+
+    @FXML
+    private void ParadigmaButtonAction(ActionEvent event) {
+        content1.getChildren().clear();
+    }
     
 }
