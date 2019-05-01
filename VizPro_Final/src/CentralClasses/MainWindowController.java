9bd9b8ce2f3a457922887b710e234f91a731674f
Joonasha <47676026+Joonasha@users.noreply.github.com>
2019-05-01 19:39:34 +0300

    etusivu päivitetty

--- VizPro_Final/src/CentralClasses/MainWindowController.java
+++ VizPro_Final/src/CentralClasses/MainWindowController.java
@@ -7,15 +7,21 @@ package CentralClasses;
 
 import CentralClasses.MainWindow;
 import com.jfoenix.controls.JFXButton;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -43,18 +49,27 @@ public class MainWindowController implements Initializable {
     public JFXButton SearchGrades;
     @FXML
     private JFXButton Login;
-    @FXML
-    private JFXButton Manual;
+
     @FXML
     private Label title;
+    @FXML
+    private ImageView logoImage;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         CreateGrade.setFocusTraversable(false);
-        Manual.setFocusTraversable(false);
         SearchGrades.setFocusTraversable(false);
         Login.setFocusTraversable(false);
         title.setText("Gradujen visualisointi\r\n   \t    työkalu");
+        FileInputStream inputstream; 
+        try {
+            inputstream = new FileInputStream("image001.jpg");
+
+        Image image = new Image(inputstream); 
+        logoImage.setImage(image);
         
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(MainWindowController.class.getName()).log(Level.SEVERE, null, ex);
+        }        
     }    
 
     @FXML
@@ -72,9 +87,5 @@ public class MainWindowController implements Initializable {
 
     @FXML
     private void LoginAction(ActionEvent event) {
-    }
-
-    @FXML
-    private void ManualAction(ActionEvent event) {
     }
 }
